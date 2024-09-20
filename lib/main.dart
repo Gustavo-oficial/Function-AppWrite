23016531081d2c1b9403e9b7e65d4506dbe9808c
gfelix <gfelix@sp.senai.br>
2024-09-20 16:13:31 -0300

    Refactorar 'main.dart' para usar a variável de ambiente de contexto para o ID do projeto e remover o arquivo 'env.json'

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'dart:io';
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:dart_appwrite/models.dart';
 
-const String databaseId = String.fromEnvironment("DATABASE_ID");
-const String collectionId = String.fromEnvironment("COLLECTION_ID");
-
 Future<dynamic> main(final context) async {
   final String? projectId = Platform.environment['PROJECT_ID']; 
+  final String? databaseId = Platform.environment['DATABASE_ID'];
+  final String? collectionId = Platform.environment['COLLECTION_ID'];
 
   final client = Client()
       .setEndpoint('https://cloud.appwrite.io/v1')
@@ -18,8 +17,8 @@ Future<dynamic> main(final context) async {
   if (context.req.path == "/storeItems") {
     try {
       final DocumentList storesItems = await databases.listDocuments(
-        databaseId: databaseId,
-        collectionId: collectionId
+        databaseId: databaseId!,
+        collectionId: collectionId!
       );
 
       dynamic items = [];
