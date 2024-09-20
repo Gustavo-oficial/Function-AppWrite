17214c04104d366d2b36e4793066e581644be820
gfelix <gfelix@sp.senai.br>
2024-09-20 11:33:05 -0300

    Create 'Store Function' and add environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,34 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:dart_appwrite/dart_appwrite.dart';
 
-// This Appwrite function will be executed every time your function is triggered
+
+const String projectId = String.fromEnvironment("PROJECT_ID"); 
+const String databaseId = String.fromEnvironment("DATABASE_ID");
+const String collectionId = String.fromEnvironment("COLLECTION_ID");
+
 Future<dynamic> main(final context) async {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
   final client = Client()
-    .setEndpoint(Platform.environment['APPWRITE_FUNCTION_API_ENDPOINT'] ?? '')
-    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'] ?? '')
-    .setKey(context.req.headers['x-appwrite-key'] ?? '');
-  final users = Users(client);
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject(projectId);
 
-  try {
-    final response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    context.log('Total users: ' + response.total.toString());
-  } catch (e) {
-    context.error('Could not list users: ' + e.toString());
-  }
+  final databases = Databases(client);
 
-  // The req object contains the request data
-  if (context.req.path == "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return context.res.text('Pong');
+  if (context.req.path == "/storeItems") {
+    try {
+      final storesItems = databases.listDocuments(
+        databaseId: databaseId,
+        collectionId: collectionId
+      );
+
+      print(storesItems);
+    } on AppwriteException catch(e) {
+        print(e);
+    }
   }
 
   return context.res.json({
     'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
+    'learn': context.process.env.API_KEY,
     'connect': 'https://appwrite.io/discord',
     'getInspired': 'https://builtwith.appwrite.io',
   });
