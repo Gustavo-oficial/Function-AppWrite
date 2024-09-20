20df76bcdcf7a5d50ed021076c51e9ea58be52a7
gfelix <gfelix@sp.senai.br>
2024-09-20 13:15:02 -0300

    Refactor 'main.dart' to use context environment variable for project ID

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:dart_appwrite/models.dart';
 
 
-const String projectId = String.fromEnvironment("PROJECT_ID"); 
 const String databaseId = String.fromEnvironment("DATABASE_ID");
 const String collectionId = String.fromEnvironment("COLLECTION_ID");
 
 Future<dynamic> main(final context) async {
+  final String projectId = context.env["PROJECT_ID"]; 
+
   final client = Client()
       .setEndpoint('https://cloud.appwrite.io/v1')
       .setProject(projectId);
