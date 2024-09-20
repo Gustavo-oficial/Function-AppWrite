ea458c44046504cbbc429a9037a1b2228f3148a5
gfelix <gfelix@sp.senai.br>
2024-09-20 12:36:46 -0300

    Refactor 'main.dart' to include project ID in error response

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ Future<dynamic> main(final context) async {
       });
     } on AppwriteException catch(e) {
       return context.res.json({
+        "project": projectId, 
         'error': e.code,
         "message": e.message
       });
