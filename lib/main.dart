45bd78c7204447876598613bb3e5e114f3ab420d
gfelix <gfelix@sp.senai.br>
2024-09-20 16:34:35 -0300

    Refatorar 'main.dart' para incluir IDs do banco de dados e da coleção na resposta

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,28 @@ Future<dynamic> main(final context) async {
   final String? projectId = Platform.environment['PROJECT_ID']; 
   final String? databaseId = Platform.environment['DATABASE_ID'];
   final String? collectionId = Platform.environment['COLLECTION_ID'];
+  final String? apiKey = Platform.environment['API_KEY'];
 
   final client = Client()
       .setEndpoint('https://cloud.appwrite.io/v1')
-      .setProject(projectId);
+      .setProject(projectId)
+      .setKey(apiKey);
 
   final databases = Databases(client);
 
   if (context.req.path == "/storeItems") {
     try {
-      final DocumentList storesItems = await databases.listDocuments(
+      DocumentList result = await databases.listDocuments(
         databaseId: databaseId!,
-        collectionId: collectionId!
+        collectionId: collectionId!,
+        queries: [],
       );
 
       return context.res.json({
+        "result": result,
         "database": databaseId,
         "collection": collectionId,
-        'items': storesItems.documents
+        'items': result.documents
       });
     } on AppwriteException catch(e) {
       return context.res.json({
