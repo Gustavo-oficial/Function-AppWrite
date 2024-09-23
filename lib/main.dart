8eccc6b9aca670bbda90702bc72b80dbb65487d8
gfelix <gfelix@sp.senai.br>
2024-09-23 14:04:56 -0300

    Refactor 'main.dart' to use query parameter instead of route parameter for item ID

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,20 @@ Future<dynamic> main(final context) async {
 
   switch (context.req.path) {
     case "/storeItems":
-      return await StoreItem.getStoreItems(
-        context: context, 
-        databases: databases
-      );
-    case "/storeItemById":
       final itemId = context.req.query['id'];
 
       if (itemId == null || itemId.isEmpty) {
-        return context.res.json({
-          'error': 'Item ID is required for fetching a specific store item',
-        }, status: 400);
+        return await StoreItem.getStoreItems(
+          context: context, 
+          databases: databases
+        );
+      }else{
+        return await StoreItem.getStoreItemById(
+          context: context, 
+          databases: databases,
+          itemId: itemId
+        );
       }
-
-      return await StoreItem.getStoreItemById(
-        context: context, 
-        databases: databases,
-        itemId: itemId
-      );
     default:
       return context.res.json({
         'motto': 'Build like a team of hundreds_',
