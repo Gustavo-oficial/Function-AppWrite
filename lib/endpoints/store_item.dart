4f9384883a6a041b06952c588e19c9f0ad5a369d
gfelix <gfelix@sp.senai.br>
2024-09-23 13:56:46 -0300

    Refatorar 'main.dart' para incluir IDs do banco de dados e da coleção na resposta

---
+++ lib/endpoints/store_item.dart
@@ -0,0 +1,54 @@
+import 'package:dart_appwrite/dart_appwrite.dart';
+import 'package:dart_appwrite/models.dart';
+
+import '../env_config.dart';
+
+class StoreItem {
+  static getStoreItems({
+    required dynamic context,  
+    required Databases databases
+  }) async{
+    try {
+      DocumentList result = await databases.listDocuments(
+        databaseId: databaseId!,
+        collectionId: storeItemColletionId!,
+        queries: [],
+      );
+
+      List<Map<String, dynamic>> items = result.documents.map((doc) => doc.toMap()).toList();
+      items.map((item) => item['data']["status"] == true).toList();
+
+      return context.res.json({
+        'items': items 
+      });
+    } on AppwriteException catch (e) {
+      return context.res.json({
+        'code': e.code,
+        "message": e.message
+      });
+    }
+  }
+
+  static getStoreItemById({
+    required dynamic context,  
+    required Databases databases,
+    required String itemId
+  }) async{
+    try {
+      Document result = await databases.getDocument(
+        databaseId: databaseId!,
+        collectionId: storeItemColletionId!,
+        documentId: itemId
+      );
+      
+      return context.res.json({
+        'item': result.toMap()
+      });
+    } on AppwriteException catch (e) {
+      return context.res.json({
+        'code': e.code,
+        "message": e.message
+      });
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:dart_appwrite/dart_appwrite.dart';
-import 'package:dart_appwrite/models.dart';
+import 'package:starter_template/endpoints/store_item.dart';
 import 'package:starter_template/env_config.dart';
 
 Future<dynamic> main(final context) async {
@@ -11,32 +11,31 @@ Future<dynamic> main(final context) async {
 
   final databases = Databases(client);
 
-  if (context.req.path == "/storeItems") {
-    try {
-      DocumentList result = await databases.listDocuments(
-        databaseId: databaseId!,
-        collectionId: storeItemColletionId!,
-        queries: [],
+  switch (context.req.path) {
+    case "/storeItems":
+      return await StoreItem.getStoreItems(
+        context: context, 
+        databases: databases
       );
+    case "/storeItemById":
+      final itemId = context.req.params['id'];
 
-      List<Map<String, dynamic>> items = result.documents.map((doc) => doc.toMap()).toList();
+      if (itemId == null || itemId.isEmpty) {
+        return context.res.json({
+          'error': 'Item ID is required for fetching a specific store item',
+        }, status: 400);
+      }
 
+      return await StoreItem.getStoreItemById(
+        context: context, 
+        databases: databases,
+        itemId: itemId
+      );
+    default:
       return context.res.json({
-        'items': items 
+        'motto': 'Build like a team of hundreds_',
+        'connect': 'https://appwrite.io/discord',
+        'getInspired': 'https://builtwith.appwrite.io',
       });
-    } on AppwriteException catch (e) {
-      return context.res.json({
-        "storeCollectionId": storeItemColletionId,
-        "project": projectId, 
-        'code': e.code,
-        "message": e.message
-      });
-    }
   }
-
-  return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
-  });
 }
